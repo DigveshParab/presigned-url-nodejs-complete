cde773fa2ea800fb40a91b7d5f06d9c2674ea3c3
Digvesh parab <159231475+DigveshParab@users.noreply.github.com>
2025-04-14 15:33:33 +0530

    Update user.ts

--- routes/user.ts
+++ routes/user.ts
@@ -10,7 +10,7 @@ const s3Client = new S3Client({
         secretAccessKey:process.env.SECRET_KEY!,
         accessKeyId:process.env.ACCESS_KEY!,
     },
-    region:"us-east-1"
+    region:process.env.AWS_REGION as string
 })
 
 // get request to get the presigned URL
@@ -19,7 +19,7 @@ router.get("/get_presigned_url",async(req,res)=>{
     let user = "testUser"
 
     const {url,fields} = await createPresignedPost(s3Client,{
-        Bucket:"testbucketforthistime",
+        Bucket:process.env.AWS_BUCKET_NAME?.toString() || "",
         Key:`backery/${user}/${Math.random()}/image.png`,
         Conditions:[
             ['content-length-range', 0, 5 * 1024 * 1024] // 5 MB max
@@ -41,20 +41,3 @@ router.get("/get_presigned_url",async(req,res)=>{
 
 
 export default router;
-
-// the presigned URL that is generated will be send to the  frontend
-// we do not handle uploading in the backend
-// as that would mean taking the image in the backend
-// our whole purpose is to avoid that
-
-
-// 2 ways to handle it 
-// 1. we provide a button where the user can click and generate the presignedURL
-    //   once thsi is done based on the availablity of presigned URL provide another button
-    //   clicking which will uplaod the images to the S3 via presigned url
-
-// 2. when the user selects the images immediatedly send them to the S3 return the image url 
-    //   This is cloudfornturl + image_path
-    //   taking it a step further show a preview with cancel button otherwise there will be unnecessary image sin our S3 bucket
-    //   lastly store the returned s3 url into the DB to access the image in the future     
-
